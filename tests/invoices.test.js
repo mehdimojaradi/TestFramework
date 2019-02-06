de5a865f1ba0b8b79577d0e3940d26d81df9e6fa
Amir Hossein <montazer60@yahoo.com>
2019-02-06 11:08:33 +0330

    write test for add item and adjust markup edit general invoice

--- tests/invoices.test.js
+++ tests/invoices.test.js
@@ -39,4 +39,73 @@ describe("Invoices", () => {
     let isVisible_dialog = await invoicePage.elementIsVisible(invoice.$edit_item_dialog);
     await expect(isVisible_dialog).toEqual(false);
   });
+
+  it("should add general invoice item", async () => {
+    let id = "11438";
+    await invoicePage.findInvoiceBy(id);
+    await invoicePage.waitForElement(invoice.$number_link);
+    await invoicePage.clickButton(invoice.$number_link);
+
+    await invoicePage.waitForElement(invoice.$add_item_dialog_button);    
+    await invoicePage.clickButton(invoice.$add_item_dialog_button);
+
+    await invoicePage.waitForElement(invoice.$add_invoice_item_itemname);
+    await invoicePage.fillElementByCss(invoice.$add_invoice_item_itemname,'item 2');
+    await invoicePage.fillElementByCss(invoice.$add_invoice_item_itemdescriotion,'description');
+    await invoicePage.fillElementByCss(invoice.$add_invoice_item_price,'100');
+    await invoicePage.fillElementByCss(invoice.$add_invoice_item_qty,'3');
+
+    await invoicePage.waitForElement(invoice.$edit_item_dialog_save_button);
+    await invoicePage.clickButton(invoice.$edit_item_dialog_save_button);
+    let isVisible_dialog = await invoicePage.elementIsVisible(invoice.$edit_item_dialog);
+    await expect(isVisible_dialog).toEqual(false);
+  });
+
+  it("should close add item dialog general invoice", async () => {
+    let id = "11438";
+    await invoicePage.findInvoiceBy(id);
+    await invoicePage.waitForElement(invoice.$number_link);
+    await invoicePage.clickButton(invoice.$number_link);
+
+    await invoicePage.waitForElement(invoice.$add_item_dialog_button);    
+    await invoicePage.clickButton(invoice.$add_item_dialog_button);
+
+    await invoicePage.waitForElement(invoice.$add_item_dialog_close_button);
+    await invoicePage.clickButton(invoice.$add_item_dialog_close_button);
+    let isVisible_dialog = await invoicePage.elementIsVisible(invoice.$add_item_dialog_close_button);
+    await expect(isVisible_dialog).toEqual(false);
+  });
+
+  it("should add adjust markup general invoice item", async () => {
+    let id = "11438";
+    await invoicePage.findInvoiceBy(id);
+    await invoicePage.waitForElement(invoice.$number_link);
+    await invoicePage.clickButton(invoice.$number_link);
+
+    await invoicePage.waitForElement(invoice.$adjust_markup_dialog_button);    
+    await invoicePage.clickButton(invoice.$adjust_markup_dialog_button);
+
+    await invoicePage.waitForElement(invoice.$adjust_markup_dialog_percentage);
+    await invoicePage.fillElementByCss(invoice.$adjust_markup_dialog_percentage,'15');
+
+    await invoicePage.waitForElement(invoice.$adjust_markup_dialog_ok_button);
+    await invoicePage.clickButton(invoice.$adjust_markup_dialog_ok_button);
+    let isVisible_dialog = await invoicePage.elementIsVisible(invoice.$adjust_markup_dialog_ok_button);
+    await expect(isVisible_dialog).toEqual(false);
+  });
+
+  it("should cancel adjust markup general invoice item", async () => {
+    let id = "11438";
+    await invoicePage.findInvoiceBy(id);
+    await invoicePage.waitForElement(invoice.$number_link);
+    await invoicePage.clickButton(invoice.$number_link);
+
+    await invoicePage.waitForElement(invoice.$adjust_markup_dialog_button);    
+    await invoicePage.clickButton(invoice.$adjust_markup_dialog_button);
+
+    await invoicePage.waitForElement(invoice.$adjust_markup_dialog_cancel_button);
+    await invoicePage.clickButton(invoice.$adjust_markup_dialog_cancel_button);
+    let isVisible_dialog = await invoicePage.elementIsVisible(invoice.$adjust_markup_dialog_cancel_button);
+    await expect(isVisible_dialog).toEqual(false);
+  });
 });
